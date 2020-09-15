6bdaa2f2817110ce2283ce0509da467277eaa6c9
Morre Thierry <Briareos@live.be>
2020-09-15 13:58:33 +0200

    Exo Indexeurs

--- GestBanque/Program.cs
+++ GestBanque/Program.cs
@@ -26,11 +26,18 @@ namespace GestBanque
                 Titulaire = titulaire
             };
 
-            courant.Depot(200);
-            courant.Retrait(500);
+            Banque TechnoBanque = new Banque()
+            {
+                Nom = "TechnoBanque" 
+            };
+
+            TechnoBanque.Ajouter(courant);
+
+            TechnoBanque["00001"].Depot(200);
+            TechnoBanque["00001"].Retrait(500);
 
 
-            Console.WriteLine(courant.Solde);
+            Console.WriteLine(TechnoBanque["00001"].Solde);
         }
     }
 }
---
+++ Models/Banque.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class Banque
+    {
+        private Dictionary<string, Courant> _comptes = new Dictionary<string, Courant>();
+        public string Nom { get; set; }
+
+        public Courant this[string numero]
+        {
+            get 
+            {
+                //_comptes.TryGetValue(numero, out Courant courant);
+                //return courant;
+                //ou
+                return _comptes[numero];
+            }
+        }
+
+        public void Ajouter(Courant compte)
+        {
+            _comptes.Add(compte.Numero, compte);
+        }
+
+        public void Supprimer(string numero)
+        {
+            _comptes.Remove(numero);
+        }
+
+    }
+}
