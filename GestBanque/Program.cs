bffddaa3e052d5113dbf2234bb8d4f4817566aec
Morre Thierry <Briareos@live.be>
2020-09-15 11:24:26 +0200

    Ajoutez des fichiers projet.

---
+++ GestBanque/Program.cs
@@ -0,0 +1,36 @@
+﻿using Models;
+using System;
+
+namespace GestBanque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Personne titulaire = new Personne();
+            //titulaire.Nom = "Doe";
+            //titulaire.Prenom = "John";
+            //titulaire.DateNaiss = new DateTime(1970, 1, 1);
+            //ou
+            Personne titulaire = new Personne()
+            {
+                Nom = "Doe",
+                Prenom = "John",
+                DateNaiss = new DateTime(1970, 1, 1)
+            };
+
+            Courant courant = new Courant()
+            {
+                Numero = "00001",
+                LigneDeCredit = 300,
+                Titulaire = titulaire
+            };
+
+            courant.Depot(200);
+            courant.Retrait(500);
+
+
+            Console.WriteLine(courant.Solde);
+        }
+    }
+}
---
+++ Models/Courant.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class Courant
+    {
+        private string _numero;
+        private double _ligneDeCredit;
+        private double _solde;
+        private Personne _titulaire;
+
+        public string Numero
+        {
+            get
+            {
+                return _numero;
+            }
+
+            set
+            {
+                _numero = value;
+            }
+        }
+
+        public double LigneDeCredit
+        {
+            get
+            {
+                return _ligneDeCredit;
+            }
+
+            set
+            {
+                if (value < 0)
+                    return; //à changer en exception par la suite
+
+                _ligneDeCredit = value;
+            }
+        }
+
+        public double Solde
+        {
+            get
+            {
+                return _solde;
+            }
+
+            private set
+            {
+                _solde = value;
+            }
+        }
+
+        public Personne Titulaire
+        {
+            get
+            {
+                return _titulaire;
+            }
+
+            set
+            {
+                _titulaire = value;
+            }
+        }
+
+        public void Depot(double montant)
+        {
+            if (!(montant > 0))
+                return;
+
+            Solde += montant;
+        }
+
+        public void Retrait(double montant)
+        {
+            if (!(montant > 0))
+                return; //à changer en exception par la suite
+
+            if (Solde - montant < -LigneDeCredit)
+                return; //à changer en exception par la suite
+
+            Solde -= montant;
+        }
+
+
+    }
+}
